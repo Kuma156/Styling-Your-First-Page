f6d01ecfd82e062ca1260d13df2c493d2a6ea96c
Kuma156 <tulamgiahuy@gmail.com>
2026-04-10 16:38:47 +0700

    add js

---
+++ script.js
@@ -0,0 +1,18 @@
+function upDate(previewPic) {
+
+    console.log("Event triggered");
+
+    console.log("Alt: " + previewPic.alt);
+    console.log("Source: " + previewPic.src);
+
+    document.getElementById("image").innerHTML = previewPic.alt;
+    document.getElementById("image").style.backgroundImage = "url('" + previewPic.src + "')";
+}
+
+function undo() {
+
+    console.log("Event triggered");
+
+    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+    document.getElementById("image").style.backgroundImage = "url('')";
+}
