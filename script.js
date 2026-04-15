7dae55cab8867c6c17d81b30692eabc5391d982f
Kuma156 <tulamgiahuy@gmail.com>
2026-04-15 08:32:53 +0700

    Final Project: Your Own Accessible Photo Gallery

--- script.js
+++ script.js
@@ -5,14 +5,64 @@ function upDate(previewPic) {
     console.log("Alt: " + previewPic.alt);
     console.log("Source: " + previewPic.src);
 
-    document.getElementById("image").innerHTML = previewPic.alt;
-    document.getElementById("image").style.backgroundImage = "url('" + previewPic.src + "')";
+    let imageDiv = document.getElementById("image");
+
+    if(!imageDiv) {
+        console.error("Element with id 'image' not found.");
+        return;
+    }
+
+    imageDiv.innerHTML = previewPic.alt;
+    imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
 }
 
 function undo() {
 
     console.log("Event triggered");
 
-    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
-    document.getElementById("image").style.backgroundImage = "url('')";
+    let imageDiv = document.getElementById("image");
+
+    if(!imageDiv) {
+        console.error("Element with id 'image' not found.");
+        return;
+    }
+
+    imageDiv.innerHTML = "Hover over an image below to display here.";
+    imageDiv.style.backgroundImage = "url('')";
 }
+
+function initGallery() {
+    console.log("Page loaded");
+
+    let images = document.querySelectorAll(".hover-img");
+    for(let i = 0; i< images.length; i++) {
+        images[i].setAttribute("tabindex", "0");
+    }
+}
+
+let items = document.querySelectorAll(".gallery-item");
+
+items.forEach(function(item) {
+    let timer;
+
+    function startEffect() {
+        console.log("Hover start");
+
+        timer = setTimeout(function() {
+            item.classList.add("zoom");
+            item.classList.add("show-caption");
+        }, 5000); // 5 seconds
+    }
+
+    function stopEffect() {
+        console.log("Hover cancelled");
+        clearTimeout(timer);
+        item.classList.remove("zoom");
+        item.classList.remove("show-caption");
+    }
+
+    item.addEventListener("mouseover", startEffect);
+    item.addEventListener("mouseleave", stopEffect);
+    item.addEventListener("focusin", startEffect);
+    item.addEventListener("focusout", stopEffect);
+});
